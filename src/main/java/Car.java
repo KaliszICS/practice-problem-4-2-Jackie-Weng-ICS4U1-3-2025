907974d8ba43f0a094999288d6349c1e0217af15
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-05-26 00:32:06 +0000

    java docs done

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,46 +1,58 @@
+/** Jackie Weng **/
+/**  Problem Set 4-2 **/
+/**  J.Kalisz **/
+
+//**  creates a "car" with paramters such as its make, model, year and price. also with getters and setters **/
 
 public class Car {
 
-private String make;
+private String make; // my instance variables
 private String model;
 private int year;
 private double price;
 
+//**  constuctor: initalizes my car with the 3 things above **/
+//* @param make: make of the car */
+//* @param model: the model of a car */
+//* @param year: the year of a car */
+//* @param price: the price of the car */
 public Car(String make, String model, int year, double price){
     this.make = make;
     this.model = model;
     this.year = year;
     this.price = price;
 }
-
+//* gets the make of the car */
+//* @return return the make of the car */
 public String getMake(){
     return this.make;
 }
-
+//* the exact same thing as above */
 public String getModel(){
     return this.model;
-}
-
+}//* the exact same thing as above */
 public int getYear(){
     return this.year;
 }
-
+//* the exact same thing as above */
 public double getPrice(){
     return this.price;
 }
-
+//* sets the cars make to a provided string */
+//* @param newMake, a string that the current make is replaced with */
 public void setMake(String newMake){
     this.make = newMake;
 }
-
+//* sets the cars mode to a provided string */
+//* @param newMake, a string that the current model is replaced with */
 public void setModel(String newModel){
     this.model = newModel;
 }
-
+//* exact same thing but with year @param newYear, a string that the year is replaced with */
 public void setYear(int newYear){
     this.year = newYear;
 }
-
+//* exact same thing but with price @param newPrice, a string that the price is replaced with */
 public void setPrice(double newPrice){
     this.price = newPrice;
 }
--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,3 +1,12 @@
+/** Jackie Weng **/
+/**  Problem Set 4-2 **/
+/**  J.Kalisz **/
+
+//**  creates a "dog" with paramters such as its weight, name, and breed. also with getters and setters **/
+//* @param breed of the dog */
+//* @param Name of the dog */
+//* @param weight of the dog */
+
 class Dog{
 private int weight;
 private String name;
@@ -9,27 +18,42 @@ public Dog(String name, String breed, int weight){
     this.weight = weight;
 }
 
+//* gets name of the pooch */
+//* @return return name of pooch as string */
 public String getName(){
     return this.name;
 }
 
+//* gets breed of the pooch */
+//* @return return breed of pooch as string */
 public String getBreed(){
     return this.breed;
 }
 
+//* gets weight of the pooch */
+//* @return return weight of pooch as string */
 public int getWeight(){
     return this.weight;
 }
 
 
+//* set name to provided string */
+//* @param the name that the current name is replaced with*/
 public void setName(String name) {
     this.name = name;
 }
+
+
+//* set breed to provided string */
+//* @param the breed that the current breed is replaced with*/
 
 public void setBreed(String breed){
     this.breed = breed;
 }
 
+//* almost the exact same thing */
+//* set weight to provided string */
+//* @param the weight that the current weight is replaced with*/
 public void setWeight(int weight){
     this.weight = weight;
 }
